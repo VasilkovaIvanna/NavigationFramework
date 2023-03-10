13a8b8841103d7a4113e4f71e8d27b518d2fca19
VasilkovaIvanna <ivanna.vasilkova@wolkov.consulting>
2023-03-10 22:04:35 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NavigationFramework
+
+A description of this package.
---
+++ Sources/NavigationFramework/NavigationFramework.swift
@@ -0,0 +1,10 @@
+public struct NavigationFramework {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+
+    public static func hello(){
+        print("D")
+    }
+}
