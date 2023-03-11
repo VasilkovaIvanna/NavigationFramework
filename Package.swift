93db44d902093631f1f4af65235dc1e8036dc95f
VasilkovaIvanna <ivanna.vasilkova@wolkov.consulting>
2023-03-11 16:47:09 +0100

    added framework build

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NavigationFramework",
+    platforms: [.macOS(.v12), .iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +19,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "NavigationFramework",
-            dependencies: []),
-        .testTarget(
-            name: "NavigationFrameworkTests",
-            dependencies: ["NavigationFramework"]),
+        .binaryTarget(name: "NavigationFramework",
+                      path: "./Sources/NavigationFramework.xcframework")
     ]
 )
---
+++ Sources/NavigationFramework.xcframework/ios-arm64/NavigationFramework.framework/Headers/NavigationFramework.h
@@ -0,0 +1,18 @@
+//
+//  NavigationFramework.h
+//  NavigationFramework
+//
+//  Created by Ivy on 11.03.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NavigationFramework.
+FOUNDATION_EXPORT double NavigationFrameworkVersionNumber;
+
+//! Project version string for NavigationFramework.
+FOUNDATION_EXPORT const unsigned char NavigationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavigationFramework/PublicHeader.h>
+
+
